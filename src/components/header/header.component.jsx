4162f31c5d384df76e772eec20a6776383505192
Ashwin-Kurlye <kurlyeashwin@gmail.com>
2021-01-06 14:10:19 +0800

    cart context addded for hidden toggle

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -13,37 +13,47 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
 import CurrentUserContext from '../../contexts/current-user/current-user.context';
+import CartContext from '../../contexts/cart/cart.context';
 
-const Header = ({ hidden }) => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
-      <Logo className='logo' />
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
-        SHOP
+const Header = () => {
+  const [hidden, setHidden] = useState(true);
+  const toggleHidden = () => {
+    return setHidden(!hidden);
+  };
+
+  return (
+    <div className='header'>
+      <Link className='logo-container' to='/'>
+        <Logo className='logo' />
       </Link>
-      <Link className='option' to='/shop'>
-        CONTACT
-      </Link>
-      <CurrentUserContext.Consumer>
-        {currentUser =>
-          currentUser ? (
-            <div className='option' onClick={() => auth.signOut()}>
-              SIGN OUT
-            </div>
-          ) : (
-            <Link className='option' to='/signin'>
-              SIGN IN
-            </Link>
-          )
-        }
-      </CurrentUserContext.Consumer>
-      <CartIcon />
+      <div className='options'>
+        <Link className='option' to='/shop'>
+          SHOP
+        </Link>
+        <Link className='option' to='/shop'>
+          CONTACT
+        </Link>
+        <CurrentUserContext.Consumer>
+          {currentUser =>
+            currentUser ? (
+              <div className='option' onClick={() => auth.signOut()}>
+                SIGN OUT
+              </div>
+            ) : (
+              <Link className='option' to='/signin'>
+                SIGN IN
+              </Link>
+            )
+          }
+        </CurrentUserContext.Consumer>
+        <CartContext.Provider value={{ hidden, toggleHidden }}>
+          <CartIcon />
+        </CartContext.Provider>
+      </div>
+      {hidden ? null : <CartDropdown />}
     </div>
-    {hidden ? null : <CartDropdown />}
-  </div>
-);
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   hidden: selectCartHidden,
